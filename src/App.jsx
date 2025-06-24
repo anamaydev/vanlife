666141cc7cb7722df285706a876c7d216ebc5b40
anamaydev <anamaydev@gmail.com>
2025-06-24 16:54:16 +0530

    chore: add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import './App.css'
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home(){
+  return (
+    <h1>Yo this is a home page</h1>
+  )
+}
+
+function About(){
+  return (
+    <h1>about page this is a</h1>
   )
 }
 
