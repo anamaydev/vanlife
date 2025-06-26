8ba6dc9f5d4448854697cbe6d3e1c94ea089f7ec
anamaydev <anamaydev@gmail.com>
2025-06-26 06:36:54 +0530

    refactor: move Header and Footer to Layout component using Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Vans from "./pages/Vans"
-import VanDetails from "./pages/VanDetails"
-import Header from "./components/Header"
-import Footer from "./components/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Vans from "./pages/Vans";
+import VanDetails from "./pages/VanDetails";
+import Layout from "./components/Layout";
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-dvh flex flex-col">
-        <Header/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />}/>
-          <Route path="/vans/:id" element={<VanDetails />} />
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />}/>
+            <Route path="/vans/:id" element={<VanDetails />} />
+          </Route>
         </Routes>
-        <Footer/>
       </div>
     </BrowserRouter>
   )
